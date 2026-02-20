760fbedacaa505179c65f51cea33b3596043e321
WebDesignerIoan <100315750+WebDesignerIoan@users.noreply.github.com>
2026-02-20 13:07:51 +0200

    Added edges and title and centered Board

--- src/main/java/hio/qss/Launcher.java
+++ src/main/java/hio/qss/Launcher.java
@@ -3,7 +3,8 @@ package hio.qss;
 import javafx.application.Application;
 
 public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(QSSApplication.class, args);
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello, World");
+    Application.launch(QSSApplication.class, args);
+  }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module hio.qss {
-    requires javafx.controls;
-    requires javafx.fxml;
+  requires javafx.controls;
+  requires javafx.fxml;
 
+  opens hio.qss to javafx.fxml;
 
-    opens hio.qss to javafx.fxml;
-    exports hio.qss;
+  exports hio.qss;
 }
