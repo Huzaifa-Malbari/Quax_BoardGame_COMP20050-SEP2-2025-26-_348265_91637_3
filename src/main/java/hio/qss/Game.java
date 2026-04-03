ff54c3cf4f5857b828f9b81b02b81265beb59091
Huzaifa Malbari <huzaifamalbari@gmail.com>
2026-04-03 19:27:28 +0100

    restored before test merge

--- src/main/java/hio/qss/Game.java
+++ src/main/java/hio/qss/Game.java
@@ -5,193 +5,191 @@ import java.util.ArrayList;
 
 public class Game {
 
-    private static int MAX_RHOMBIS = 10;
-    private static int MAX_OCTAGONS = 11;
+  private static int MAX_RHOMBIS = 10;
+  private static int MAX_OCTAGONS = 11;
 
-    private boolean isBlack;
-    private boolean blackWins;
-    private boolean gameOver;
+  private boolean isBlack;
+  private boolean blackWins;
+  private boolean gameOver;
 
-    private BoardCell[][] ocells;
-    private BoardCell[][] rcells;
-    private int moveCount; // added by Ioan
+  private BoardCell[][] ocells;
+  private BoardCell[][] rcells;
+  private int moveCount; // added by Ioan
 
+  public Game() {
+    isBlack = true;
+    ocells = new BoardCell[11][11];
+    rcells = new BoardCell[10][10];
+    blackWins = false;
+    gameOver = false;
+    moveCount = 0;
+  }
 
-    public Game() {
-        isBlack = true;
-        ocells = new BoardCell[11][11];
-        rcells = new BoardCell[10][10];
-        blackWins = false;
-        gameOver = false;
-        moveCount = 0;
+  public boolean placeCell(Boolean isRhombic, int row, int col) {
+
+    if (isRhombic) {
+      if (rcells[row][col] != null) {
+        return false;
+      }
+      BoardCell cell = createCell(row, col, isRhombic);
+      rcells[row][col] = cell;
+      updateChains(cell);
+    } else {
+      if (ocells[row][col] != null) {
+        return false;
+      }
+      BoardCell cell = createCell(row, col, isRhombic);
+      ocells[row][col] = cell;
+      updateChains(cell);
+    }
+    moveCount++;
+    isBlack = !isBlack;
+    return true;
+  }
+
+  private BoardCell createCell(int row, int col, boolean isRhombic) {
+    BoardCell cell;
+    if (isRhombic) {
+      cell = new BoardCell(true, (isBlack) ? CellStatus.B : CellStatus.W, row, col);
+    } else {
+      cell = new BoardCell(false, (isBlack) ? CellStatus.B : CellStatus.W, row, col);
     }
 
-    public boolean placeCell(Boolean isRhombic, int row, int col) {
+    return cell;
+  }
 
-        if (isRhombic) {
-            if (rcells[row][col] != null || gameOver) {
-                return false;
-            }
-            BoardCell cell = createCell(row, col, isRhombic);
-            rcells[row][col] = cell;
-            updateChains(cell);
-        }else {
-            if (ocells[row][col] != null || gameOver) {
-                return false;
-            }
-            BoardCell cell = createCell(row, col, isRhombic);
-            ocells[row][col] = cell;
-            updateChains(cell);
+  public boolean isBlack() {
+    return isBlack;
+  }
+
+  public boolean isGameOver() {
+    return gameOver;
+  }
+
+  public boolean isBlackWins() {
+    return blackWins;
+  }
+
+  private void updateChains(BoardCell thisCell) {
+
+    int row = thisCell.getRow();
+    int col = thisCell.getCol();
+    boolean isRhombic = thisCell.getRhombic();
+
+    if (isRhombic) {
+      thisCell = rcells[row][col];
+    }
+
+    ArrayList<BoardCell> neighbours = getNeighbours(thisCell);
+    ArrayList<CellGroup> groups = new ArrayList<CellGroup>();
+    CellGroup maxGroup = new CellGroup();
+    for (BoardCell neighbour : neighbours) {
+      if (neighbour != null && neighbour.getStatus().equals(thisCell.getStatus())
+          && !groups.contains(neighbour.getGroup())) {
+        groups.add(neighbour.getGroup());
+        if (groups.size() > 0 && groups.getLast().size() > maxGroup.size()) {
+          maxGroup = groups.getLast();
         }
-        moveCount++;
-        isBlack = !isBlack;
-        return true;
+      }
     }
 
-    private BoardCell createCell(int row, int col, boolean isRhombic) {
-        BoardCell cell;
-        if (isRhombic) {
-            cell = new BoardCell(true, (isBlack) ? CellStatus.B : CellStatus.W, row, col);
-        }else {
-            cell = new BoardCell(false, (isBlack) ? CellStatus.B : CellStatus.W, row, col);
+    if (maxGroup.size() != 0) {
+      groups.remove(maxGroup);
+    }
+    for (CellGroup group : groups) {
+      maxGroup.merge(group);
+    }
+
+    maxGroup.addCell(thisCell);
+    maxGroup.setFurthest(thisCell);
+
+    checkForWin();
+  }
+
+  private void checkForWin() {
+
+    if (isBlack) {
+      for (int i = 0; i < MAX_OCTAGONS; i++) {
+        if (ocells[0][i] == null || ocells[0][i].getStatus().equals(CellStatus.W)) {
+          continue;
         }
-
-
-
-        return cell;
-    }
-
-    public boolean isBlack() {
-        return isBlack;
-    }
-
-    public boolean isGameOver() {
-        return gameOver;
-    }
-
-    public boolean isBlackWins() {
-        return blackWins;
-    }
-
-    private void updateChains(BoardCell thisCell) {
-
-        int row = thisCell.getRow();
-        int col = thisCell.getCol();
-        boolean isRhombic = thisCell.getRhombic();
-
-        if (isRhombic) {
-            thisCell = rcells[row][col];
+        if (ocells[0][i].getGroup().getFurthest().getRow() == MAX_OCTAGONS - 1) {
+          gameOver = true;
+          blackWins = true;
+          break;
         }
-
-        ArrayList<BoardCell> neighbours = getNeighbours(thisCell);
-        ArrayList<CellGroup> groups = new ArrayList<CellGroup>();
-        CellGroup maxGroup = new CellGroup();
-        for (BoardCell neighbour : neighbours) {
-            if (neighbour != null && neighbour.getStatus().equals(thisCell.getStatus()) && !groups.contains(neighbour.getGroup())) {
-                groups.add(neighbour.getGroup());
-                if (groups.size() > 0 && groups.getLast().size() > maxGroup.size()) {
-                    maxGroup = groups.getLast();
-                }
-            }
+      }
+    } else {
+      for (int i = 0; i < MAX_OCTAGONS; i++) {
+        if (ocells[i][0] == null || ocells[i][0].getStatus().equals(CellStatus.B)) {
+          continue;
         }
-
-        if (maxGroup.size() != 0) {
-            groups.remove(maxGroup);
+        if (ocells[i][0].getGroup().getFurthest().getCol() == MAX_OCTAGONS - 1) {
+          gameOver = true;
+          blackWins = false;
+          break;
         }
-        for (CellGroup group : groups) {
-            maxGroup.merge(group);
-        }
-
-        maxGroup.addCell(thisCell);
-        maxGroup.setFurthest(thisCell);
-
-        checkForWin();
+      }
     }
+  }
 
-    private void checkForWin() {
+  public ArrayList<BoardCell> getNeighbours(BoardCell thisCell) {
 
-       if (isBlack) {
-           for (int i = 0; i < MAX_OCTAGONS; i++) {
-               if (ocells[0][i] == null || ocells[0][i].getStatus().equals(CellStatus.W)) {
-                   continue;
-               }
-               if (ocells[0][i].getGroup().getFurthest().getRow() == MAX_OCTAGONS - 1) {
-                   gameOver = true;
-                   blackWins = true;
-                   break;
-               }
-           }
-       }else {
-           for (int i = 0; i < MAX_OCTAGONS; i++) {
-               if (ocells[i][0] == null || ocells[i][0].getStatus().equals(CellStatus.B)) {
-                   continue;
-               }
-               if (ocells[i][0].getGroup().getFurthest().getCol() == MAX_OCTAGONS - 1) {
-                   gameOver = true;
-                   blackWins = false;
-                   break;
-               }
-           }
-       }
-    }
+    int row = thisCell.getRow();
+    int col = thisCell.getCol();
+    boolean isRhombic = thisCell.getRhombic();
+    ArrayList<BoardCell> neighbours = new ArrayList<>();
 
-    public ArrayList<BoardCell> getNeighbours(BoardCell thisCell) {
+    if (isRhombic) {
+      thisCell = rcells[row][col];
 
-        int row = thisCell.getRow();
-        int col = thisCell.getCol();
-        boolean isRhombic = thisCell.getRhombic();
-        ArrayList<BoardCell> neighbours = new ArrayList<>();
+      neighbours.add(ocells[row][col]);
+      neighbours.add(ocells[row][col + 1]);
+      neighbours.add(ocells[row + 1][col]);
+      neighbours.add(ocells[row + 1][col + 1]);
 
-        if (isRhombic) {
-            thisCell  = rcells[row][col];
+    } else {
 
-            neighbours.add(ocells[row][col]);
-            neighbours.add(ocells[row][col + 1]);
-            neighbours.add(ocells[row + 1][col]);
-            neighbours.add(ocells[row + 1][col + 1]);
+      // check neighboring octagons
+      if (row - 1 >= 0) {
+        neighbours.add(ocells[row - 1][col]);
+      }
+      if (row + 1 < MAX_OCTAGONS) {
+        neighbours.add(ocells[row + 1][col]);
+      }
+      if (col - 1 >= 0) {
+        neighbours.add(ocells[row][col - 1]);
+      }
+      if (col + 1 < MAX_OCTAGONS) {
+        neighbours.add(ocells[row][col + 1]);
+      }
 
-        } else {
+      // check neighboring rhombises
 
-            // check neighboring octagons
-            if (row - 1 >= 0) {
-                neighbours.add(ocells[row - 1][col]);
-            }
-            if (row + 1 < MAX_OCTAGONS) {
-                neighbours.add(ocells[row + 1][col]);
-            }
-            if (col - 1 >= 0) {
-                neighbours.add(ocells[row][col - 1]);
-            }
-            if (col + 1 < MAX_OCTAGONS) {
-                neighbours.add(ocells[row][col + 1]);
-            }
-
-            // check neighboring rhombises
-
-            if (col - 1 >= 0) {
-                if (row < MAX_RHOMBIS) {
-                    neighbours.add(rcells[row][col - 1]);
-                }
-                if (row - 1 >= 0) {
-                    neighbours.add(rcells[row - 1][col - 1]);
-                }
-            }
-            if (col < MAX_RHOMBIS) {
-                if (row < MAX_RHOMBIS) {
-                    neighbours.add(rcells[row][col]);
-                }
-                if (row - 1 >= 0) {
-                    neighbours.add(rcells[row - 1][col]);
-                }
-
-            }
+      if (col - 1 >= 0) {
+        if (row < MAX_RHOMBIS) {
+          neighbours.add(rcells[row][col - 1]);
+        }
+        if (row - 1 >= 0) {
+          neighbours.add(rcells[row - 1][col - 1]);
+        }
+      }
+      if (col < MAX_RHOMBIS) {
+        if (row < MAX_RHOMBIS) {
+          neighbours.add(rcells[row][col]);
+        }
+        if (row - 1 >= 0) {
+          neighbours.add(rcells[row - 1][col]);
         }
 
-        return neighbours;
+      }
     }
 
-    public int getMoveCount() {
-        return moveCount;
-    }
+    return neighbours;
+  }
+
+  public int getMoveCount() {
+    return moveCount;
+  }
 
 }
--- src/test/java/hio/qss/QSSControllerTest.java
+++ src/test/java/hio/qss/QSSControllerTest.java
@@ -1,51 +1,21 @@
 package hio.qss;
 
-import javafx.scene.Node;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Polygon;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class QSSControllerTest {
-    @Test
-   void setPlayerTurnTextTest() {
+  @Test
+  void setPlayerTurnTextTest() {
 
-        QSSController qssController = new QSSController();
+    QSSController qssController = new QSSController();
 
-        assertEquals("Black's Turn", qssController.getTurnText());
+    assertEquals("Black's Turn", qssController.getTurnText());
 
-        qssController.game.placeCell(false, 0, 0); // switches turn from Black to White
-        assertEquals("White's Turn", qssController.getTurnText());
-   }
+    qssController.game.placeCell(false, 0, 0); // switches turn from Black to White
+    assertEquals("White's Turn", qssController.getTurnText());
 
-   @Test
-    void restartButtonTest() {
-       QSSController qssController = new QSSController();
-
-       qssController.game.placeCell(false, 0, 0);
-       qssController.game.placeCell(false, 0, 1);
-
-       qssController.game = new Game();
-
-       assertEquals(0, qssController.game.getMoveCount());
-       assertTrue(qssController.game.isBlack());
-       assertFalse(qssController.game.isGameOver());
-       assertFalse(qssController.game.isBlackWins());
-   }
-
-    @Test
-    void displayWinner() {
-        QSSController qssController = new QSSController();
-    for(int row = 0; row <= 10; row++) {
-        qssController.game.placeCell(false, row, 0);
-        if(!qssController.game.isGameOver()){
-            qssController.game.placeCell(false, row, 1);
-        }
-    }
-    assertTrue(qssController.game.isGameOver());
-    assertTrue(qssController.game.isBlackWins());
-    }
+  }
 
   /*
    * Unit tests added by Ioan - Check if pie rule button should be shown in
